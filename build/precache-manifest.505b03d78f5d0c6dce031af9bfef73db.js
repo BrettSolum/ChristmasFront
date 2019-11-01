4a1f37215f9c12895a9d390b062bbd180b7f2803
Solum <bsolum@agreserves.com>
2019-11-01 09:44:57 -0600

    added build again

---
+++ build/precache-manifest.505b03d78f5d0c6dce031af9bfef73db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6660fca8fbe6e38b7112a829b326fa11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f932d41b468915ac0c2b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9d92246733df011f1c63",
+    "url": "/static/js/2.20983305.chunk.js"
+  },
+  {
+    "revision": "f932d41b468915ac0c2b",
+    "url": "/static/js/main.1bacbd0f.chunk.js"
+  },
+  {
+    "revision": "958dc228f07b5eaa5d94",
+    "url": "/static/js/runtime-main.fff54aa9.js"
+  },
+  {
+    "revision": "ac29ad2c65702ae977198eb98a19d9d8",
+    "url": "/static/media/XmasTree.ac29ad2c.png"
+  }
+]);
